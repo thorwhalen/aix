3aff1931d8b7577e3be1007a7eead8a778498e10
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-09-04 13:25:38 +0200

    refactor: Organize imports and enhance utility functions in util.py; add new Jupyter notebook for model data exploration

--- aix/util.py
+++ aix/util.py
@@ -1,5 +1,22 @@
 """Utils for AIX."""
 
-from config2py import simple_config_getter
+import os
+from importlib.resources import files
+from functools import partial
 
-get_config = simple_config_getter("aix")
+from config2py import simple_config_getter, get_app_data_folder, process_path
+
+
+pkg_name = 'aix'
+
+get_config = simple_config_getter(pkg_name)
+
+data_files = files(pkg_name) / "data"
+templates_files = data_files / "templates"
+app_data_dir = os.environ.get(
+    f"{pkg_name.upper()}_APP_DATA_DIR",
+    get_app_data_folder(pkg_name)
+)
+app_data_dir = process_path(app_data_dir, ensure_dir_exists=True)
+djoin = partial(os.path.join, app_data_dir)
+model_info_dir = process_path(djoin("model_info"), ensure_dir_exists=True)
