0fbbcd801926c42114535471172d8b01fdcfc83c
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-10-11 09:53:37 -0700

    refactor: Rename get_app_data_folder to get_app_config_folder

--- aix/util.py
+++ aix/util.py
@@ -4,7 +4,7 @@ import os
 from importlib.resources import files
 from functools import partial
 
-from config2py import simple_config_getter, get_app_data_folder, process_path
+from config2py import simple_config_getter, get_app_config_folder, process_path
 
 
 pkg_name = 'aix'
@@ -14,8 +14,7 @@ get_config = simple_config_getter(pkg_name)
 data_files = files(pkg_name) / "data"
 templates_files = data_files / "templates"
 app_data_dir = os.environ.get(
-    f"{pkg_name.upper()}_APP_DATA_DIR",
-    get_app_data_folder(pkg_name)
+    f"{pkg_name.upper()}_APP_DATA_DIR", get_app_config_folder(pkg_name)
 )
 app_data_dir = process_path(app_data_dir, ensure_dir_exists=True)
 djoin = partial(os.path.join, app_data_dir)
