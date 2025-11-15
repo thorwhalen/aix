5019614ce56748b1feab7364385e68d42dd20db6
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2025-11-15 13:19:26 -0800

    refactor: remove legacy ML tools

--- aix/np.py
+++
@@ -1,9 +0,0 @@
-"""numpy"""
-
-from guide.tools import submodule_callables
-
-import numpy
-
-for _ in list(dict.fromkeys(submodule_callables(numpy))):
-    if hasattr(_, "__name__"):
-        locals()[_.__name__] = _
